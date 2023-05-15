c756c27e1ec9ff7accf9eaf2dff5a8ace71d23b1
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2023-05-15 14:53:33 +0200

    components: adding menu components

---
+++ components/loggedin_menu.php
@@ -0,0 +1,14 @@
+<header class="p-3 text-bg-dark">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" class="nav-link px-2 text-white">Domov</a></li>
+            </ul>
+
+            <div class="text-end">
+                <p class="d-inline px-2">Ahoj <?php echo $_SESSION["first_name"] . " " . $_SESSION["surname"]; ?>!</p>
+                <a href="/authentification/logout.php" class="btn btn-warning">Odhlásiť sa</a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ components/loggedout_menu.php
@@ -0,0 +1,14 @@
+<header class="p-3 text-bg-dark">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" class="nav-link px-2 text-white">Semestrálne zadanie</a></li>
+            </ul>
+
+            <div class="text-end">
+                <a href="/authentification/index.php" class="btn btn-outline-light me-2">Prihlásiť sa</a>
+                <a href="/authentification/register.php" class="btn btn-warning">Registrovať sa</a>
+            </div>
+        </div>
+    </div>
+</header>
