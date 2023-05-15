6c6ce262c4d5e0f38ad7fe525bc2f481bbc40d7d
Juraj Revaj <59011895+jRevaj@users.noreply.github.com>
2023-05-15 14:55:24 +0200

    student gui + functionality

---
+++ components/student_index.php
@@ -0,0 +1,212 @@
+<?php
+// Get available files from db
+try {
+    $stmt = $db->prepare("SELECT * FROM files f WHERE f.active = 1");
+    $stmt->execute();
+    $available_files = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Remove files that have already been used
+    $stmt = $db->prepare("SELECT file_id FROM test WHERE student_id = :user_id");
+    $stmt->bindParam(':user_id', $_SESSION["user_id"]);
+    $stmt->execute();
+    $used_files = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($available_files as $key => $file) {
+        foreach ($used_files as $used_file) {
+            if ($file["id"] == $used_file["file_id"]) {
+                unset($available_files[$key]);
+            }
+        }
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// Get available tasks from db
+try {
+    $stmt = $db->prepare("SELECT t.id, f.source, t.answer  FROM test t INNER JOIN files f ON t.file_id = f.id WHERE student_id = :user_id AND t.points_gained IS NULL");
+    $stmt->bindParam(':user_id', $_SESSION["user_id"]);
+    $stmt->execute();
+    $student_tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// Get task history from db
+try {
+    $stmt = $db->prepare("SELECT t.id, t.points_gained, f.source, f.points  FROM test t INNER JOIN files f ON t.file_id = f.id WHERE student_id = :user_id AND t.points_gained IS NOT NULL");
+    $stmt->bindParam(':user_id', $_SESSION["user_id"]);
+    $stmt->execute();
+    $student_history = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+// Generate exercises
+if (!empty($_POST) && !empty($_POST["user_id"]) && !empty($_POST["files"])) {
+    $parsed_files = array();
+
+    foreach ($_POST["files"] as $file) {
+        $parsed_files[] = array(
+            "id" => explode(":", $file)[0],
+            "source" => explode(":", $file)[1],
+            "tasks" => parseFile($file)
+        );
+    }
+
+    // save tasks to db
+    try {
+        $stmt = $db->prepare("INSERT INTO test (student_id, question, image, solution, file_id) VALUES (:user_id, :task, :image, :solution, :file_id)");
+
+        foreach ($parsed_files as $file) {
+            foreach ($file["tasks"] as $task) {
+                $stmt->bindParam(':user_id', $_POST["user_id"]);
+                $stmt->bindParam(':task', $task["task"]);
+                $stmt->bindParam(':image', $task["image"]);
+                $stmt->bindParam(':solution', $task["solution"]);
+                $stmt->bindParam(':file_id', $file["id"]);
+                $stmt->execute();
+            }
+        }
+
+        // refresh page
+        header("Refresh:0");
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+?>
+<main class="mb-5">
+    <section class="container w-25 mt-5">
+        <h2 class="mb-3 text-center">Generovanie príkladov</h2>
+        <form action="#" method="post" class="d-flex flex-column justify-content-center">
+            <input type="hidden" name="user_id" value="<?php echo $_SESSION["user_id"]; ?>">
+            <div class="form-check">
+                <p class="mb-3">Vyber sadu príkladov z ktorej chceš generovať:</p>
+                <?php
+                foreach ($available_files as $file) {
+                    echo '<div class="form-check d-flex flex-row justify-content-start ms-3">';
+                    echo '<input class="form-check-input me-2" type="checkbox" name="files[]" value="' . $file["id"] . ':' . $file["source"] . '" id="' . $file["source"] . '">';
+                    echo '<label class="form-check-label" for="' . $file["source"] . '">';
+                    echo $file["source"];
+                    echo '</label>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Vygenerovať príklady</button>
+        </form>
+    </section>
+
+    <section class="container w-50 mt-5">
+        <h2 class="text-center">Neriešené príklady</h2>
+        <table id="available" class="table table-striped mt-2" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Zdroj príkladu</th>
+                    <th>Číslo príkladu</th>
+                    <th>Možnosti</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $counter = 1;
+                $previous = null;
+                foreach ($student_tasks as $task) {
+                    if ($previous) {
+                        if ($task["source"] == $previous["source"]) {
+                            $counter++;
+                        } else {
+                            $counter = 1;
+                        }
+                    }
+
+                    echo '<tr>';
+                    echo '<td>' . $task["source"] . '</td>';
+                    echo '<td>' . $counter . '</td>';
+                    echo '<td>';
+                    echo '<a href="taskDetail.php?id=' . $task["id"] . '" class="btn btn-primary">Riešiť</a>';
+                    if ($task["answer"]) {
+                        echo '<a href="taskDetail.php?id=' . $task["id"] . '" class="btn btn-success ms-2">Odovzdať</a>';
+                    }
+                    echo '</td>';
+                    echo '</tr>';
+                    $previous = $task;
+                }
+                ?>
+            </tbody>
+        </table>
+    </section>
+
+    <section class="container w-50 mt-5">
+        <h2 class="text-center">Odovzdané príklady</h2>
+        <table id="available" class="table table-striped mt-2" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Zdroj príkladu</th>
+                    <th>Číslo príkladu</th>
+                    <th>Získané body</th>
+                    <th>Možnosti</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $counter = 1;
+                $previous = null;
+                foreach ($student_history as $task) {
+                    if ($previous) {
+                        if ($task["source"] == $previous["source"]) {
+                            $counter++;
+                        } else {
+                            $counter = 1;
+                        }
+                    }
+
+                    echo '<tr>';
+                    echo '<td>' . $task["source"] . '</td>';
+                    echo '<td>' . $counter . '</td>';
+                    echo '<td>' . $task["points_gained"] . '/' . $task["points"] . '</td>';
+                    echo '<td>';
+                    echo '<a href="taskSolution.php?id=' . $task["id"] . '" class="btn btn-primary">Zobraziť riešenie</a>';
+                    echo '</td>';
+                    echo '</tr>';
+                    $previous = $task;
+                }
+                ?>
+            </tbody>
+        </table>
+    </section>
+</main>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#available').DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "Nie sú dostupné žiadne dáta",
+                "info": "Zobrazených _START_ až _END_ z _TOTAL_ záznamov",
+                "infoEmpty": "Zobrazených 0 až 0 z 0 záznamov",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Zobraz _MENU_ záznamov",
+                "loadingRecords": "Načítavam...",
+                "processing": "",
+                "search": "Vyhľadať:",
+                "zeroRecords": "Nenašli sa žiadne zodpovedajúce záznamy",
+                "paginate": {
+                    "first": "Prvá",
+                    "last": "Posledná",
+                    "next": "Nasledujúca",
+                    "previous": "Predchádzajúca"
+                },
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                }
+            }
+        });
+    });
+</script>
---
+++ taskDetail.php
@@ -0,0 +1,144 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require_once('config.php');
+require_once('utils/parse_tex_file.php');
+
+if (!isset($_SESSION["user_id"])) {
+    header("location: login.php");
+}
+
+if ($_SESSION["type"] != "Student") {
+    header("location: index.php");
+}
+
+try {
+    $id = $_GET['id'];
+    $query = 'SELECT * FROM `test` WHERE `id` = ' . $id . ' LIMIT 1';
+    $stmt = $db->query($query);
+    $task = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (empty($task)) {
+        header("location: index.php");
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.3/r-2.4.0/datatables.min.css" />
+    <link href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap5.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    
+    <style>
+        .container > span:nth-child(2) > span:nth-child(1) > span:nth-child(2) {
+            display: inline-block !important;
+            margin-inline: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        include 'components/loggedin_menu.php';
+    } elseif (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+        include 'components/loggedin_menu.php';
+    } else {
+        include 'components/loggedout_menu.php';
+    }
+    ?>
+
+    <main>
+        <section class="container mt-5 w-50">
+            <h2>Zadanie:</h2>
+            <span>
+                <?php
+                echo str_replace("$", "$$", $task["question"]);
+                if ($task["image"] != null) {
+                    echo '<img src="uploads/images/' . basename($task["image"]) . '" class="img-fluid mt-5" alt="task image">';
+                }
+
+                ?>
+            </span>
+
+
+            <h2 class="mt-5">Vaše riešenie:</h2>
+            <div class="w-50">
+                <form>
+                    <math-field id="answer" style="width:100%;">
+                        <?php
+                        if ($task["answer"] != null) {
+                            echo $task["answer"];
+                        }
+                        ?>
+                    </math-field>
+                    <a class="btn btn-primary mt-3" onclick="saveAnswer();" type="submit">Uložiť</a>
+                    <button disabled id="submit-btn" type="submit" class="btn btn-success mt-3">Odovzdať</button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <script src="https://unpkg.com/mathlive"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {
+            function checkMathField() {
+                const answer = $('#answer').val();
+
+                if (answer.length > 0) {
+                    $('#submit-btn').prop('disabled', false);
+                } else {
+                    $('#submit-btn').prop('disabled', true);
+                }
+            }
+
+            checkMathField(); // Check immediately on page load
+
+            // Then check whenever the math-field's content changes
+            $('#answer').on('input', checkMathField);
+        });
+    </script>
+    <script>
+        window.addEventListener('DOMContentLoaded',
+            () => MathLive.renderMathInDocument()
+        );
+
+        function saveAnswer() {
+            let answer = document.getElementById("answer").value;
+            let id = <?php echo $task["id"]; ?>;
+            fetch("utils/save_answer.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    answer: answer,
+                    id: id
+                })
+            }).then(response => response.json()).then(data => {
+                console.log(data);
+            })
+        }
+    </script>
+</body>
+
+</html>
