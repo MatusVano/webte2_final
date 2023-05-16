6d14f3f46551a03500b165fcf3d0ba07b23958cc
Marek Belis <marek.belis810@gmail.com>
2023-05-16 14:06:39 +0200

    link to studentInstr. in menu

--- components/loggedin_menu.php
+++ components/loggedin_menu.php
@@ -3,6 +3,7 @@
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="index.php" class="nav-link px-2 text-white">Domov</a></li>
+                <li><a href="studentInstructions.php" class="nav-link px-2 text-white">Návod</a></li>
             </ul>
 
             <div class="text-end">
