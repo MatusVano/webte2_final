79c1740ffca8e570829321093bc3c41048d99969
MatusVano <xvanom@stuba.sk>
2023-05-18 12:23:03 +0200

    final commit

--- checkAnswer.php
+++ checkAnswer.php
@@ -1,34 +1,31 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+function checkAns($poly1,$poly2){
+  $poly1 = latexToMaxima($poly1);
+  $poly2 = latexToMaxima($poly2);
+  
+  echo $poly1;
+  echo $poly2;
+  $maxima_executable = '/usr/bin/maxima'; // cesta k maxime
 
-$poly1 = "((7*s+10))/(2*s^3+11*s^2+12*s+10)";
-$poly2 = "((7*s+10))/(2*s^3+11*s^2+12*s+10)";
-$maxima_executable = '/usr/bin/maxima'; // cesta k maxime
-
-$maxima_command = sprintf('(%s) - (%s);', $poly1, $poly2);
-
-$maxima_output = shell_exec($maxima_executable . ' -b -q -r "' . $maxima_command . '"');
-echo $maxima_output;
-$matches = [];
-preg_match('/\(%o\d+\)\s*(-?\d+\.?\d*)/', $maxima_output, $matches);
-$outcome = isset($matches[1]) ? floatval($matches[1]) : null;
-
-echo $outcome;
-
-if ($outcome == "0") {
-    echo "The expressions are equal"; //zapise plny počer
-} else {
-    echo "The expressions are not equal"; //zapise 0
+  $maxima_command = sprintf('(%s) - (%s);', $poly1, $poly2);
+  
+  $maxima_output = shell_exec($maxima_executable . ' -b -q -r "' . $maxima_command . '"');
+  echo $maxima_output;
+  $matches = [];
+  preg_match('/\(%o\d+\)\s*(-?\d+\.?\d*)/', $maxima_output, $matches);
+  $outcome = isset($matches[1]) ? floatval($matches[1]) : null;
+  
+  echo $outcome;
+  
+  if ($outcome == "0") {
+      echo "The expressions are equal"; //zapise plny počer
+  } else {
+      echo "The expressions are not equal"; //zapise 0
+  }
 }
-    
-//$command = "echo 'is($poly1 = $poly2);' | maxima";
-//$output = shell_exec($command);
-//echo $output;
 
 function latexToMaxima($latex) {
-   
+    
     $latex = str_replace('\begin{equation*}', '', $latex);
     $latex = str_replace('\end{equation*}', '', $latex); 
     $latex = str_replace('\left', '', $latex);
@@ -42,40 +39,15 @@ function latexToMaxima($latex) {
     $latex = str_replace('\\', '', $latex);
     $latex = str_replace('\cdot', '*', $latex);
     $latex = preg_replace('/([0-9]+)s/', '$1*s', $latex);
-    
+    $latex = preg_replace('/([0-9]+)t/', '$1*t', $latex);
+    $latex = preg_replace('/([^a-zA-Z0-9_])e/', '$1*e', $latex);
+    $latex = str_replace('(t-6)', '*(t-6)', $latex);
+    $latex = str_replace('(t-4)', '*(t-4)', $latex);
+    $latex = str_replace('(t-7)', '*(t-7)', $latex);
+      
+    //$latex = str_replace('\eta', '', $latex);
     
     return $latex;
 }
-
-$latex2 = "\begin{equation*} \dfrac{7s+10}{2s^3+11s^2+12s+10} \end{equation*}";
-
-$maxima = latexToMaxima($latex2);
-
-
-echo "<br>";
-echo $maxima; 
-
-echo "<br>";
-
-require_once('config.php');
-
-$sql = "SELECT * FROM test";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// potiahni z databazy solution a answer, daj ich do funkcie 
-if (count($rows) > 0) {
-    foreach ($rows as $row) {
-      echo "RAW solution". $row['solution'] . "<br>";
-      echo "Parsed solution".latexToMaxima($row['solution']) . "<br>";
-      echo "Answer from student".$row['answer'] . "<br>";
-      echo "Parsed answer".latexToMaxima($row['answer']) . "<br>";
-
-      //porovnavanie
-    }
-  } else {
-    echo "No results found";
-  }
 ?>
 
